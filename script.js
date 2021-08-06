2195fc20534f4ad2a6fb13952266de6eef7e4386
CEED87 <ceed210787@gmail.com>
2021-08-06 23:10:08 +0300

    home_work TENNIS

---
+++ script.js
@@ -0,0 +1,17 @@
+const racketLeft = document.querySelector('.racket');
+const racketRight = racketLeft.nextElementSibling;
+const circle =document.querySelector('circle');
+
+
+
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode === 16) {
+
+        console.log(e.keyCode)
+    }
+    if (e.keyCode === 17) {
+        console.log(e.keyCode)
+    }
+
+    
+});
